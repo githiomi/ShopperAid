d49ab8d13855aee535bf10b4b2e539f12a951c76
githiomi <danielgithiomi@gmail.com>
2020-11-08 17:26:35 +0300

    Login page layout file design complete

--- app/src/main/java/com/githiomi/onlineshoppingassistant/Ui/LoginActivity.java
+++ app/src/main/java/com/githiomi/onlineshoppingassistant/Ui/LoginActivity.java
@@ -3,14 +3,44 @@ package com.githiomi.onlineshoppingassistant.Ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.githiomi.onlineshoppingassistant.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity {
+
+//    TAG
+    private static final String TAG = LoginActivity.class.getSimpleName();
+
+//    Binding widgets using butter knife
+    @BindView(R.id.edEmail) TextInputEditText wUserEmail;
+    @BindView(R.id.edPassword) TextInputEditText wUserPassword;
+    @BindView(R.id.btnLogin) Button wLoginButton;
+    @BindView(R.id.tvToSignUp) TextView mToSignUp;
+    @BindView(R.id.tvForgotPassword) TextView wForgotPassword;
+
+//    Local variables
+    // Date entry
+    private String mEmail;
+    private String mPassword;
+    // Firebase
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Binding widgets using butter knife
+        ButterKnife.bind(this);
+
+        
     }
 }
