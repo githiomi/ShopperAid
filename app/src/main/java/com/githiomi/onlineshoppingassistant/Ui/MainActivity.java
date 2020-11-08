f33057d7e19e9411238c2e2f4ab135ea8b12aa4f
githiomi <danielgithiomi@gmail.com>
2020-11-08 14:56:16 +0300

    Landing page loads and opens login

--- app/src/main/java/com/githiomi/onlineshoppingassistant/Ui/MainActivity.java
+++ app/src/main/java/com/githiomi/onlineshoppingassistant/Ui/MainActivity.java
@@ -2,7 +2,9 @@ package com.githiomi.onlineshoppingassistant.Ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.githiomi.onlineshoppingassistant.R;
 
@@ -12,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        load();
     }
+
+    // Method that will delay before tha app opens.
+    public void load(){
+
+        Handler progressHandler = new Handler();
+
+        progressHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startApp();
+            }
+        }, 3000);
+    }
+
+    // Method that will take user to the login page.
+    public void startApp(){
+
+        Intent toLogin = new Intent(this, LoginActivity.class);
+        toLogin.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(toLogin);
+        finish();
+
+    }
+
 }
